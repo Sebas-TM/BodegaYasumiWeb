be99f613e96285605c1b57629abbded126e026d5
benjamin-brll <benjaminrenzollt@gmail.com>
2022-10-06 22:42:42 -0500

    link a login, utilidad de navegacion

--- Bodega-Yasumi/src/components/Navigation.jsx
+++ Bodega-Yasumi/src/components/Navigation.jsx
@@ -1,3 +1,5 @@
+import { Link } from "react-router-dom"
+
 const Navigation = () => {
     return(
         <>
@@ -13,7 +15,7 @@ const Navigation = () => {
                 </div>
                 <nav className="barra__linea__3 ">
                     <a className="navegacion__enlace" href="productos.html">ayuda <i className='icon header__icon bx bxs-help-circle' ></i></a>
-                    <a className="navegacion__enlace" href="">ingresar <i className='icon header__icon bx bxs-lock-alt' ></i></a>
+                    <Link className="navegacion__enlace" to="login">ingresar <i className='icon header__icon bx bxs-lock-alt' ></i></Link>
                     <a className="navegacion__enlace navegacion__enlace--rojo" href="">registrar <i className='icon header__icon bx bxs-user-plus' ></i></a>
                     <button className="icon__bars">
                         <div></div>
