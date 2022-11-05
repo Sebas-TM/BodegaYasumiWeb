666ce8ceb69b3eb3cde67182d8e7504ad98a3437
Sebas-TM <sebastimoteo3@gmail.com>
2022-11-04 19:35:47 -0500

    register modificado

--- Bodega-Yasumi/src/pages/Register/Register.jsx
+++ Bodega-Yasumi/src/pages/Register/Register.jsx
@@ -4,26 +4,51 @@ const Register = () => {
     return (
         <div className="register-wrap">
             <div className="register-html">
-                <div className='headerreg'><label htmlFor="tab-2" className="tab">REGISTRARSE</label></div>
+                <div className='headerreg'><label htmlFor="tab-2" className="tab">Bodega Yasumi</label></div>
                 <form className="register-form">
                     <div className="sign-up-htm">
                         <div className="group">
-                            <label htmlFor="user" className="label">Usuario</label>
+                            <label htmlFor="user" className="label">Nombre:</label>
                             <input id="user" type="text" className="input" />
                         </div>
                         <div className="group">
-                            <label htmlFor="pass" className="label">Contraseña</label>
+                            <label htmlFor="user" className="label">Apellido paterno:</label>
+                            <input id="user" type="text" className="input" />
+                        </div>
+                        <div className="group">
+                            <label htmlFor="user" className="label">Apellido Materno:</label>
+                            <input id="user" type="text" className="input" />
+                        </div>
+                        <div className="group">
+                            <label htmlFor="user" className="label">Teléfono:</label>
+                            <input id="user" type="number" className="input" />
+                        </div>
+                        <div className="group">
+                            <label htmlFor="user" className="label">DNI:</label>
+                            <input id="user" type="number" className="input" />
+                        </div>
+                        <div className="group">
+                            <label htmlFor="user" className="label">Sexo:</label>
+                            <select name="" id="" className='input'>
+                            <option disabled value="0">Seleccionar</option>
+                            <option value="1">Masculino</option>
+                            <option value="2">Femenino</option>
+                            </select>
+                        </div>
+                        <div className="group">
+                            <label htmlFor="pass" className="label">Correo:</label>
+                            <input id="pass" type="email" className="input" data-type="password" />
+                        </div>
+                        <div className="group">
+                            <label htmlFor="pass" className="label">Contraseña:</label>
                             <input id="pass" type="password" className="input" data-type="password" />
                         </div>
                         <div className="group">
-                            <label htmlFor="pass" className="label">Repetir contraseña</label>
-                            <input id="pass" type="password" className="input" data-type="password" />
-                        </div>
-                        <div className="group">
-                            <label htmlFor="pass" className="label">Correo electrónico</label>
-                            <input id="pass" type="text" className="input" />
-                        </div>
-                        <div className="group">
+                            <label htmlFor="pass" className="label">Confirmar contraseña:</label>
+                            <input id="pass" type="password" className="input" />
+                        </div>                        
+                    </div>
+                    <div className="group">
                             <input type="submit" className="button button__register" value="Registrar" />
                         </div>
                         <div className="hr"></div>
@@ -32,7 +57,6 @@ const Register = () => {
                                 <Link to='/login'>¿Ya tiene una cuenta?</Link>
                             </label>
                         </div>
-                    </div>
                 </form>
             </div>
         </div>
